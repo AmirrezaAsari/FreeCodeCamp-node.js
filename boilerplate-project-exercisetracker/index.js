cfacdb7525fb1ec6d1b155ec81e04ccb648e6397
unknown <amirreza.asari2@gmail.com>
2024-01-07 01:37:55 +0330

    add exercise and get log

--- boilerplate-project-exercisetracker/index.js
+++ boilerplate-project-exercisetracker/index.js
@@ -8,6 +8,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 let users = [];
+let exercise = [];
+let logs = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -25,6 +27,77 @@ app.post("/api/users", (req, res) => {
 
 app.get("/api/users", (req, res) => {
   res.json(users);
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const { description, duration, date } = req.body;
+  const userId = req.params._id;
+  let user = users.find((user) => user._id === userId);
+
+  if (!user) {
+    return res.json({ error: "User not found" });
+  }
+
+  const newExercise = {
+    username: user.username,
+    description: description,
+    duration: parseInt(duration),
+    date: date ? new Date(date).toDateString() : new Date().toDateString(),
+    _id: user._id,
+  };
+
+  user = { ...user, ...newExercise };
+  exercise.push(newExercise);
+  logs.push(newExercise);
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    description: description,
+    duration: newExercise.duration,
+    date: newExercise.date,
+  });
+});
+
+app.get("/api/users/:_id/logs", (req, res) => {
+  const userId = req.params._id;
+  const user = users.find((user) => user._id === userId);
+
+  if (!user) {
+    return res.json({ error: "User not found" });
+  }
+
+  const { from, to, limit } = req.query;
+  let filteredLogs = logs.filter((log) => log._id === userId);
+
+  if (from) {
+    filteredLogs = filteredLogs.filter(
+      (log) => new Date(log.date) >= new Date(from),
+    );
+  }
+
+  if (to) {
+    filteredLogs = filteredLogs.filter(
+      (log) => new Date(log.date) <= new Date(to),
+    );
+  }
+
+  if (limit) {
+    filteredLogs = filteredLogs.slice(0, parseInt(limit));
+  }
+
+  const response = {
+    _id: user._id,
+    username: user.username,
+    count: filteredLogs.length,
+    log: filteredLogs.map((logItem) => ({
+      description: logItem.description,
+      duration: Number(logItem.duration),
+      date: logItem.date,
+    })),
+  };
+
+  res.json(response);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
